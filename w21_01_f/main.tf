b966e666c317db330518130340406130461f627a
Luciano Marcondes <lucianobm@gmail.com>
2023-04-07 17:41:55 -0300

    w21_01_f

--- w21_01_f/main.tf
+++ w21_01_f/main.tf
@@ -1,8 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+module "asg" {
+  source = "./modules/asg"
 }
---
+++ w21_01_f/modules/asg/variables.tf
@@ -0,0 +1,5 @@
+variable "apache_instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "Apache"
+}
