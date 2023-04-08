7291216ac4f31ff26bb9a4d903f29d896cf14cdf
Luciano Marcondes <lucianobm@gmail.com>
2023-04-07 22:02:00 -0300

    w21_02_a

--- w21_02_a/security-group.tf
+++ w21_02_a/security-group.tf
@@ -1,3 +1,29 @@
+resource "aws_security_group" "apache-alb-sg" {
+  name        = "apache-alb-sg"
+  description = "Allow 22 and 80"
+  # vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.apache_instance_name
+  }
+}
+
 resource "aws_security_group" "apache" {
   name        = "apache"
   description = "Allow 22 and 80"
